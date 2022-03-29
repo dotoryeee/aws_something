b79b4123eee69f3a56d4876404570a168f993404
dotoryeee <bayer@kakao.com>
2022-03-30 00:31:40 +0900

    add func: select old date threshold

--- musinsa.py
+++ musinsa.py
@@ -2,10 +2,23 @@ from os import access
 import boto3
 import datetime
 import csv
+import argparse
 
 session = boto3.Session(profile_name="musinsa")
 iam = session.client("iam")
 today = datetime.datetime.now(datetime.timezone.utc)
+
+
+def getArgs():
+    try:
+        parser = argparse.ArgumentParser()
+        parser.add_argument(
+            "old", help="insert 'old' threshold(int)", type=int, default=0, nargs="?"
+        )
+        args = parser.parse_args()
+        return args.old
+    except:
+        return int(0)
 
 
 def getUserList():
@@ -15,8 +28,8 @@ def getUserList():
         for user in iam_users:
             user_names.append(user["UserName"])
         return user_names
-    except:
-        print("FAIL: get uset list")
+    except Exception as e:
+        print(f"FAIL: get user list{e}")
 
 
 def getAccessKeys(user_names):
@@ -30,20 +43,21 @@ def getAccessKeys(user_names):
                 except:
                     pass  # pass if user don't have access key
         return access_key_list
-    except:
-        print("FAIL: get access key info")
+    except Exception as e:
+        print(f"FAIL: get access key info{e}")
 
 
-def appendHowOld(access_key_list):
+def appendHowOld(access_key_list, old_target):
     returnData = []
     try:
         for access_key_info in access_key_list:
             date_diff = today - access_key_info["CreateDate"]
-            access_key_info["HowOld"] = date_diff.days
-            returnData.append(access_key_info)
+            if date_diff.days >= old_target:
+                access_key_info["HowOld"] = date_diff.days
+                returnData.append(access_key_info)
         return returnData
-    except:
-        print("FAIL: append accesskey old info")
+    except Exception as e:
+        print(f"FAIL: append accesskey old info{e}")
 
 
 def makeCSV(access_key_list):
@@ -62,14 +76,16 @@ def makeCSV(access_key_list):
             )
         f.close()
         return "success"
-    except:
-        return "FAIL: making CSV"
+    except Exception as e:
+        print(f"FAIL: making CSV {e}")
 
 
 def main():
+    old_target = getArgs()
+    print(f"select accesskeys older than {old_target} day(s)")
     user_names = getUserList()
     access_key_list = getAccessKeys(user_names)
-    access_key_list = appendHowOld(access_key_list)
+    access_key_list = appendHowOld(access_key_list, old_target)
     make_file_result = makeCSV(access_key_list)
     print(make_file_result)
 
